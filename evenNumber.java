69abd60b1edadc168785ff891e9596d20078e45b
Jaydeep Patil <121282321+Jaydeep3580@users.noreply.github.com>
2024-11-29 00:12:28 +0530

    Add files via upload

---
+++ evenNumber.java
@@ -0,0 +1,15 @@
+package com.jdp;
+
+//#WAP to print all even numbers from 103 to 157
+public class evenNumber {
+	
+	void even() {
+
+	for(int i = 103;i<=157;i++) {
+		if(i%2==0) {
+			System.out.println(i);
+		}
+	}
+  }
+}
+
---
+++ evenNumberTest.java
@@ -0,0 +1,13 @@
+package com.jdp;
+
+public class evenNumberTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		evenNumber en = new evenNumber();
+		en.even();
+
+	}
+
+}
